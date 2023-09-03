957d1c63e89e823c326975e86b07c6e6481ca4f2
LIUBA <romanliuba15@gmail.com>
2023-09-04 01:38:07 +0200

    lesson2-1

---
+++ src/main/java/de/telran/lesson2/Task1.java
@@ -0,0 +1,24 @@
+package de.telran.lesson2;
+
+public class Task1 {
+    public static void main(String[] args) {
+
+        char mycharacter = 'G';
+        int mynumber = 89;
+        byte myByte = 4;
+        short myShort = 56;
+        float myFloat = 4.7333436F;
+        double myDouble = 4.355453532;
+        long myLong = 12121;
+
+
+        System.out.println("char: " + mycharacter );
+        System.out.println("int: " + mynumber );
+        System.out.println("byte: " + myByte );
+        System.out.println("short: " + myShort );
+        System.out.println("float: " + myFloat );
+        System.out.println("double: " + myDouble );
+        System.out.println("long: " + myLong );
+        ;
+    }
+}
