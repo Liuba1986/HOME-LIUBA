177405ee924f54fd98542faade7223682564f6d1
LIUBA <romanliuba15@gmail.com>
2023-09-26 23:08:51 +0200

    HomeWorkJAVA19.09.23

---
+++ src/main/java/de/telran/Home/Task1.java
@@ -0,0 +1,40 @@
+package de.telran.Home;
+
+import java.util.Random;
+//Создайте массив из 8 случайных целых чисел из интервала [1;50]
+//
+//Выведите массив на консоль в строку.
+//
+//Замените каждый элемент с нечетным индексом на ноль.
+//
+//Снова выведете массив на консоль в отдельной строке.
+
+public class Task1 {
+    public static void main(String[] args) {
+        // Создаем массив из 8 случайных целых чисел из интервала [1;50]
+        int[] array = new int[8];
+        Random random = new Random();
+        for (int i = 0; i < array.length; i++) {
+            array[i] = random.nextInt(50) + 1;
+        }
+
+        // Выводим исходный массив на консоль в строку
+        System.out.print("Исходный массив: ");
+        for (int num : array) {
+            System.out.print(num + " ");
+        }
+        System.out.println(); // Переходим на новую строку
+
+        // Заменяем каждый элемент с нечетным индексом на ноль
+        for (int i = 1; i < array.length; i += 2) {
+            array[i] = 0;
+        }
+
+        // Выводим измененный массив на консоль в отдельной строке
+        System.out.print("Измененный массив: ");
+        for (int num : array) {
+            System.out.print(num + " ");
+        }
+        System.out.println(); // Переходим на новую строку
+    }
+}
---
+++ src/main/java/de/telran/Home/Task2.java
@@ -0,0 +1,43 @@
+package de.telran.Home;
+
+import java.util.Random;
+
+//Создайте массив из 5 случайных целых чисел из интервала [10;99]
+//
+//Выведите его на консоль в строку.
+//
+//Определите и выведите на экран сообщение о том, является ли
+//массив строго возрастающей последовательностью.
+public class Task2 {
+    public static void main(String[] args) {
+        int[] array = new int[5];
+        Random random = new Random();
+
+        // Заполняем массив случайными числами в интервале [10; 99]
+        for (int i = 0; i < array.length; i++) {
+            array[i] = random.nextInt(90) + 10; // Генерируем число от 10 до 99
+        }
+
+        // Выводим массив на консоль
+        System.out.print("Массив: ");
+        for (int num : array) {
+            System.out.print(num + " ");
+        }
+
+        // Проверяем, является ли массив строго возрастающей последовательностью
+        boolean isIncreasing = true;
+        for (int i = 1; i < array.length; i++) {
+            if (array[i] <= array[i - 1]) {
+                isIncreasing = false;
+                break;
+            }
+        }
+
+        // Выводим результат на экран
+        if (isIncreasing) {
+            System.out.println("\nМассив является строго возрастающей последовательностью.");
+        } else {
+            System.out.println("\nМассив не является строго возрастающей последовательностью.");
+        }
+    }
+}
